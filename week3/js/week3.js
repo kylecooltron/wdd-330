a9b5adc3f25b5754f244eceb22245eb4f1a255a8
kylecooltron <kylejcoulon@gmail.com>
2022-10-01 21:17:08 -0600

    added more week 3 notes

--- week3/js/week3.js
+++ week3/js/week3.js
@@ -1,14 +1,11 @@
 
-function followMouse(event){
-  this.style.top = e.pageY;
-}
 
 const homeBase = document.querySelector(".home-base");
 const elementsList = [];
 
 function inHomeBase(element){
-    if(parseInt(element.style.left) + element.offsetWidth <= 150){
-      if(parseInt(element.style.top) + element.offsetHeight <= 150){
+    if(parseInt(element.style.left) + element.offsetWidth <= 210){
+      if(parseInt(element.style.top) + element.offsetHeight <= 210){
         return true;
       }
     }
@@ -19,36 +16,37 @@ function inHomeBase(element){
 window.addEventListener('load', (event) => {
   
   const parentDiv = document.querySelector(".parent-element");
-  const childrenToMake = 10;
+  const childrenToMake = 4;
   const childrenTypes = ["p","a","b","h2","h3","button","div"];
 
   for(let i=0; i<childrenToMake; i++){
     let randomType = childrenTypes[Math.floor(Math.random() * childrenTypes.length)];
     let element = document.createElement(randomType);
     element.classList.add("child-element");
-    element.style.left = (Math.random() * (parentDiv.offsetWidth*0.9)) + "px";
-    element.style.top = (Math.random() * (parentDiv.offsetHeight*0.9)) + "px";
-    element.innerHTML = element.tagName;
+    element.style.left = (Math.random() * (parentDiv.offsetWidth*0.8)) + "px";
+    element.style.top = (Math.random() * (parentDiv.offsetHeight*0.8)) + "px";
+    element.textContent = `<${element.tagName}>`;
     element.style.fontSize = "25px";
     parentDiv.appendChild(element);
 
     while(inHomeBase(element)){
-      element.style.left = (Math.random() * (parentDiv.offsetWidth*0.9)) + "px";
-      element.style.top = (Math.random() * (parentDiv.offsetHeight*0.9)) + "px";
+      element.style.left = (Math.random() * (parentDiv.offsetWidth*0.8)) + "px";
+      element.style.top = (Math.random() * (parentDiv.offsetHeight*0.8)) + "px";
     }
 
     elementsList.push(element);
 
-    element.addEventListener('click', (event) => {
+
+    const clickEvent = (event) => {
       event.target.classList.toggle("selected");
       if(element.classList.contains("selected")){
         event.target.style.color = "purple";
       }else{
         event.target.style.color = "black";
       }
-    });
+    }
 
-    element.addEventListener('mouseout', (event) => {
+    const mouseOutEvent = (event) => {
       // highlight the mouseenter target
       if(event.target.classList.contains("selected")){
         let x_pos = event.clientX - (parentDiv.getBoundingClientRect().left + window.scrollX);
@@ -58,8 +56,6 @@ window.addEventListener('load', (event) => {
           y_pos -= 20;
         }
         event.target.style.top = y_pos - (event.target.offsetHeight*0.5) + "px";
-        
-        
 
         let allIn = true;
         for(el of elementsList){
@@ -70,12 +66,35 @@ window.addEventListener('load', (event) => {
         if(allIn == true){
           homeBase.style.backgroundColor = "aqua";
           homeBase.textContent = "GREAT WORK!";
+
+          for(el of elementsList){
+            el.classList.remove("selected");
+            el.removeEventListener('click',clickEvent);
+            el.removeEventListener('mouseout',mouseOutEvent);
+          
+          el.style.color = "white";
+          }
+
         }
       }
-    });
+    }
+
+    element.addEventListener('click', clickEvent);
+    element.addEventListener('mouseout', mouseOutEvent);
 
 
   }
 });
 
+document.addEventListener('keypress', (event) => console.log(`You pressed the ${event.key} character`));
+
+
+ulElement = document.getElementById('list');
+liElement = document.querySelector('#list li');
+// capturing
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul (parent fires event first)'),true);
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),true);
 
